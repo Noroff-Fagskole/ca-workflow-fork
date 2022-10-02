c749a8fd51af488c396235ec48b86668ca2050cf
SolveigRebnord <91594496+SolveigRebnord@users.noreply.github.com>
2022-10-02 15:21:44 +0200

    Profiles.html with all profiles and my friends
    Added follow/unfollow -working but need the html to change in process. Learn adjustHTML?

--- js/endpoints/api.js
+++ js/endpoints/api.js
@@ -11,7 +11,13 @@ const ALL_POSTS_URL = API_BASE_URL + "api/v1/social/posts";
 const ALL_POSTS_ADDITIONAL_URL = API_BASE_URL + "api/v1/social/posts/?_author=true&_comments=true&reactions=true";
 
 
+const ALL_PROFILES_URL = API_BASE_URL + "api/v1/social/profiles/";
+
+
 
 //const paramByID = "/api/v1/social/posts/229"
 
-export {SIGN_UP_URL, LOG_IN_URL, ALL_POSTS_URL, ALL_POSTS_ADDITIONAL_URL};
+const queryStringProfileInfo = "?_posts=true&_following=true&_followers=true";
+
+
+export {SIGN_UP_URL, LOG_IN_URL, ALL_POSTS_URL, ALL_POSTS_ADDITIONAL_URL, ALL_PROFILES_URL, queryStringProfileInfo};
